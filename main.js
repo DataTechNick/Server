7d9a57d9b5e78d6e3484d00581b23d69706164c4
DataTechNick <www.ondrasokol@seznma.cz>
2018-01-30 13:04:14 +0100

    30.1.18 13:04

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var io = require('socket.io')();
+const io = require('socket.io')();
 
-io.on('connection', function(socket){
+io.on('connection', (socket) => {
     console.log(socket.id);
     socket.emit("socketId", socket.id);
 });
